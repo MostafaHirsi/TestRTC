66cca8104c992c56ebc39838c56cb30220422123
MostafaHirsi <mostafa_hirsi@outlook.com>
2016-07-17 19:09:00 +0100

    TESTRTC Commit 2

--- src/web/custom-typings.d.ts
+++ src/web/custom-typings.d.ts
@@ -42,9 +42,6 @@ import * as _ from 'lodash'
  *
  */
 
-// support NodeJS modules without type definitions
-declare module "*";
-
 // Extra variables that live on Global that will be replaced by webpack DefinePlugin
 declare var ENV: string;
 declare var HMR: boolean;
